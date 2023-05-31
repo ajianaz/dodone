402088d7402e6593a6627e51d9045efbb6749623
Aji Anaz <aji.anaz@gmail.com>
2023-05-31 12:42:56 +0700

    on boarding page DONE

--- lib/app/modules/onboarding/views/onboarding_view.dart
+++ lib/app/modules/onboarding/views/onboarding_view.dart
@@ -1,11 +1,13 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last
 
-import 'dart:ui';
-
-import 'package:dodone/app/routes/app_pages.dart';
+import 'package:dodone/config/colors.dart';
+import 'package:dodone/config/constant.dart';
+import 'package:dodone/utils/app_asset.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rive/rive.dart';
 
 import '../controllers/onboarding_controller.dart';
@@ -15,54 +17,74 @@ class OnboardingView extends GetView<OnboardingController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          RiveAnimation.asset(
-            'assets/animations/shapes.riv',
-          ),
-          BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
-            child: Container(
-              decoration: BoxDecoration(color: Colors.black.withOpacity(0.1)),
-            ),
-          ),
-          // Positioned.fill(
-          //   child: Align(
-          //     alignment: Alignment.centerLeft,
-          //     child: BackdropFilter(
-          //       filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
-          //       child: Container(
-          //         height: 200,
-          //         margin: EdgeInsets.symmetric(horizontal: 16),
-          //         decoration: BoxDecoration(
-          //             color: Colors.black.withOpacity(0.2),
-          //             borderRadius: BorderRadius.circular(12)),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          Positioned(
-            bottom: 50,
-            right: 50,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12), // <-- Radius
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 36.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                AppAsset.logo('urtask-logo.png'),
+                height: 110,
+              ),
+              Expanded(
+                flex: 1,
+                child: RiveAnimation.asset(
+                  'assets/animations/task-checklist.riv',
+                  artboard: "Order History_nested_comp_0",
+                  fit: BoxFit.scaleDown,
                 ),
               ),
-              onPressed: () {
-                Get.toNamed(Routes.HOME);
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Get Started"),
-                  Icon(Icons.chevron_right_sharp),
-                ],
+              Text(
+                "Manage\nYour\nTask With",
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48.w,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
-            ),
-          )
-        ],
+              Text(
+                APPNAME,
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    color: primaryColor,
+                    fontSize: 48.w,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: 8.w),
+                child: ElevatedButton(
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 12.w, vertical: 10.h),
+                    child: Text(
+                      "Let's Start",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50),
+                    backgroundColor: primaryColor,
+                    foregroundColor: bgColor,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 16.h,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
+const primaryColor = Color(0xFF6CD4B5);
 const bgColor = Color(0xff0A121A);
